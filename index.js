cd55c45f0b52532b2f59d5cc42a50b45c54ee599
subron57 <subron57@hanmail.net>
2020-10-05 13:37:36 +0900

    config

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://subron57:cs123qwe@@subron57.o59cn.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) =>{
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
